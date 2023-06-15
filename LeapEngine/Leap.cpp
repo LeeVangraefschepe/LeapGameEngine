83e846d8f1b132bcd71b4fe8aa7819cccada8358
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2023-06-15 23:06:30 +0200

    Fixed GLFW not being included
    Added simple window creation

--- LeapEngine/Leap.cpp
+++ LeapEngine/Leap.cpp
@@ -2,7 +2,43 @@
 
 #include <iostream>
 
+#include "glfw3.h"
+
 void leap::LeapEngine::Run()
 {
 	std::cout << "Engine startup\n";
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Poll for and process events */
+        glfwPollEvents();
+
+        /* Render here */
+        glClearColor(0.2f, 0.7f, 0.5f, 1.0f);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 }
