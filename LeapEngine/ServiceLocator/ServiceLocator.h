668c2c5e234e76d4aff3a36b1193288866e83d38
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2023-06-20 23:21:04 +0200

    Added service locator and set up audio
    Added ServiceLocator class
    Added audio setup to the LeapEngine

---
+++ LeapEngine/ServiceLocator/ServiceLocator.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+
+namespace leap
+{
+	namespace audio
+	{
+		class AudioSystem;
+		class DefaultAudioSystem;
+	}
+
+	class ServiceLocator final
+	{
+	public:
+		static audio::AudioSystem& GetAudio();
+		template <typename T>
+		static void RegisterAudioSystem();
+	private:
+		static std::unique_ptr<audio::AudioSystem> m_pAudioSystem;
+		static std::unique_ptr<audio::DefaultAudioSystem> m_pDefaultAudioSystem;
+	};
+
+	template<typename T>
+	inline void ServiceLocator::RegisterAudioSystem()
+	{
+		static_assert(std::is_base_of<leap::audio::AudioSystem, T>::value);
+		m_pAudioSystem = std::make_unique<T>();
+	}
+}
