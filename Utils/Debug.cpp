7d63fd909843d345b9308db65615200ed80321da
LEEVGS <lee.vgs@hotmail.com>
2023-10-13 16:44:29 +0200

    Added source location to the debugger
    User can now see what file & line called the debugger.

--- Utils/Debug.cpp
+++ Utils/Debug.cpp
@@ -2,44 +2,33 @@
 #include <iostream>
 #include <ctime>
 #include <stdexcept>
-
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+#include <windows.h>
 
 bool leap::Debug::m_IsFileLogging { false };
 bool leap::Debug::m_IsThrowingOnError { true };
 
-void leap::Debug::Log(const char* message)
+void leap::Debug::Log(const char* message, const std::source_location& location)
 {
 	const auto time = GetTime();
-	std::cout << BOLDWHITE << time << ' ' << message << '\n' << RESET;
+	std::cout << BOLDWHITE << time << ' ' << message << '\n';
+    PrintCallStack(location);
+    std::cout << RESET << '\n';
 }
 
-void leap::Debug::LogWarning(const char* message)
+void leap::Debug::LogWarning(const char* message, const std::source_location& location)
 {
     const auto time = GetTime();
-    std::cout << BOLDYELLOW << time << ' ' << message << '\n' << RESET;
+    std::cout << BOLDYELLOW << time << ' ' << message << '\n';
+    PrintCallStack(location);
+    std::cout << RESET << '\n';
 }
 
-void leap::Debug::LogError(const char* message)
+void leap::Debug::LogError(const char* message, const std::source_location& location)
 {
     const auto time = GetTime();
-    std::cout << BOLDRED << time << ' ' << message << '\n' << RESET;
+    std::cout << BOLDRED << time << ' ' << message << '\n';
+    PrintCallStack(location);
+    std::cout << RESET << '\n';
 
     if (!m_IsThrowingOnError) return;
 
@@ -66,4 +55,9 @@ std::string leap::Debug::GetTime()
     strftime(timeString, sizeof(timeString), "[%H:%M:%S]", &timeinfo);
 
     return std::string{timeString};
+}
+
+void leap::Debug::PrintCallStack(const std::source_location& location)
+{
+    std::cout << "** " << location.file_name() << " line " << location.line() << ":" << location.column() << '\n';
 }
--- Utils/Debug.h
+++ Utils/Debug.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <source_location>
 
 namespace leap
 {
@@ -8,13 +9,13 @@ namespace leap
 	public:
 		Debug() = delete;
 
-		static void Log(const std::string& message) { Log(message.c_str()); }
-		static void LogWarning(const std::string& message) { LogWarning(message.c_str()); }
-		static void LogError(const std::string& message) { LogError(message.c_str()); }
+		static void Log(const std::string& message, const std::source_location& location = std::source_location::current()) { Log(message.c_str(), location); }
+		static void LogWarning(const std::string& message, const std::source_location& location = std::source_location::current()) { LogWarning(message.c_str(), location); }
+		static void LogError(const std::string& message, const std::source_location& location = std::source_location::current()) { LogError(message.c_str(), location); }
 
-		static void Log(const char* message);
-		static void LogWarning(const char* message);
-		static void LogError(const char* message);
+		static void Log(const char* message, const std::source_location& location = std::source_location::current());
+		static void LogWarning(const char* message, const std::source_location& location = std::source_location::current());
+		static void LogError(const char* message, const std::source_location& location = std::source_location::current());
 
 		static void SetFileLogger(bool enabled);
 		static void SetThrowingOnError(bool enabled);
@@ -23,5 +24,26 @@ namespace leap
 		static bool m_IsFileLogging;
 		static bool m_IsThrowingOnError;
 		static std::string GetTime();
+		static void PrintCallStack(const std::source_location& location);
+
+#pragma region Color codes
+		inline static const char* RESET = "\033[0m";         /* Default */
+		inline static const char* BLACK = "\033[30m";        /* Black */
+		inline static const char* RED = "\033[31m";        /* Red */
+		inline static const char* GREEN = "\033[32m";        /* Green */
+		inline static const char* YELLOW = "\033[33m";        /* Yellow */
+		inline static const char* BLUE = "\033[34m";        /* Blue */
+		inline static const char* MAGENTA = "\033[35m";        /* Magenta */
+		inline static const char* CYAN = "\033[36m";        /* Cyan */
+		inline static const char* WHITE = "\033[37m";        /* White */
+		inline static const char* BOLDBLACK = "\033[1m\033[30m"; /* Bold Black */
+		inline static const char* BOLDRED = "\033[1m\033[31m"; /* Bold Red */
+		inline static const char* BOLDGREEN = "\033[1m\033[32m"; /* Bold Green */
+		inline static const char* BOLDYELLOW = "\033[1m\033[33m"; /* Bold Yellow */
+		inline static const char* BOLDBLUE = "\033[1m\033[34m"; /* Bold Blue */
+		inline static const char* BOLDMAGENTA = "\033[1m\033[35m"; /* Bold Magenta */
+		inline static const char* BOLDCYAN = "\033[1m\033[36m"; /* Bold Cyan */
+		inline static const char* BOLDWHITE = "\033[1m\033[37m"; /* Bold White */
+#pragma endregion 
 	};
 }
