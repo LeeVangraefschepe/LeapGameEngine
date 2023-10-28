cd9709a418a101129405a09298ad5b76766aa2f1
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2023-10-28 21:18:31 +0200

    Added summaries to the TerrainComponent functions

--- LeapEngine/Components/RenderComponents/TerrainComponent.h
+++ LeapEngine/Components/RenderComponents/TerrainComponent.h
@@ -23,10 +23,29 @@ namespace leap
 		TerrainComponent& operator=(const TerrainComponent& other) = delete;
 		TerrainComponent& operator=(TerrainComponent&& other) = delete;
 
+		/// <summary>
+		/// Set the size of the terrain in vertices. 
+		/// E.g. Given a size of 1000, a terrain of 1000x1000 vertices with 1 meter spacing between them gets created.
+		/// Do not call this function more then once, or you will fill your memory with unused textures
+		/// </summary>
 		void SetSize(unsigned int size);
+
+		/// <summary>
+		/// Set the heightmap texture for this terrain. A terrain mesh will be generated with the same size as the given texture.
+		/// E.g. Given a texture of size 1024x1024, a terrain of 1024x1024 vertices with 1 meter spacing between them gets created.
+		/// </summary>
 		void SetTexture(graphics::ITexture* pTexture);
 
+		/// <summary>
+		/// Get the internal height map.
+		/// This height map is only valid when the heightmap is initialized using the SetSize function.
+		/// Changes to this height map only have visual impact after calling TerrainComponent::ApplyHeights;
+		/// </summary>
 		std::vector<float>& GetHeights();
+
+		/// <summary>
+		/// Apply any changes made in the internal height map to the height map texture.
+		/// </summary>
 		void ApplyHeights();
 
 	private:
