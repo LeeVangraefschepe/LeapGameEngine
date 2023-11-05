4ada779bc29c784cd4e05e022f87a24753cf8f64
LEEVGS <lee.vgs@hotmail.com>
2023-11-05 01:25:20 +0100

    Added compile option for no console

--- UnnamedAdventureGame/main.cpp
+++ UnnamedAdventureGame/main.cpp
@@ -1,3 +1,4 @@
+#include <Windows.h>
 #include "Leap.h"
 #include "GameContext/GameContext.h"
 #include "GameContext/Logger/ImGuiLogger.h"
@@ -7,9 +8,13 @@
 int main()
 {
 	leap::GameContext::GetInstance().AddLogger<leap::ImGuiLogger>();
-
-	leap::LeapEngine engine{1280, 720, "Leap game engine"};
+	leap::LeapEngine engine{ 1280, 720, "Leap game engine" };
 	leap::SceneManager::GetInstance().AddScene("Test scene", unag::MainMenuScene::Load);
 	engine.Run(60);
 	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	main();
 }
