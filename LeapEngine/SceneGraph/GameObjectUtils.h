8525660279ff8edc595fe25277d39e76e167c28a
Rhidian De Wit <rhidiandewit@gmail.com>
2023-11-17 17:39:16 +0100

    fix compile error regaridng std::max

--- LeapEngine/SceneGraph/GameObjectUtils.h
+++ LeapEngine/SceneGraph/GameObjectUtils.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <algorithm>
 #include <string_view>
 
 namespace leap::GOutils
 {
-	namespace /*Detail*/
+	namespace Detail
 	{
 		template<typename T>
 		constexpr std::string_view WrappedTypename()
@@ -16,16 +15,22 @@ namespace leap::GOutils
 			return __PRETTY_FUNCTION__;
 			#endif
 		}
+
+		constexpr size_t Max(const size_t A, const size_t B)
+		{
+			return A < B ? B : A;
+		}
 	}
 
 	// Reference for TypeName: https://stackoverflow.com/questions/35941045/can-i-obtain-c-type-names-in-a-constexpr-way
 	template <typename T>
 	constexpr std::string_view ConstexprTypeName()
 	{
-		constexpr std::string_view wrappedName{ WrappedTypename<T>() };
+		constexpr std::string_view wrappedName{ Detail::WrappedTypename<T>() };
 
 		constexpr size_t endOfType{ wrappedName.find_last_of('>') };
-		constexpr size_t beginOfType{ std::max(wrappedName.find_last_of(' '), wrappedName.find_last_of('<')) };
+		// I would use std::max, but it doesn't work for some inexplicable reason
+		constexpr size_t beginOfType{ Detail::Max(wrappedName.find_last_of(' '), wrappedName.find_last_of('<')) };
 
 		return wrappedName.substr(beginOfType + 1, endOfType - beginOfType - 1);
 	}
