e33fac8604d61515009d7b89b4e0caf8a146e4b5
LEEVGS <lee.vgs@hotmail.com>
2023-11-23 22:14:18 +0100

    Cleaned up main

--- UnnamedAdventureGame/main.cpp
+++ UnnamedAdventureGame/main.cpp
@@ -4,16 +4,10 @@
 #include "GameContext/GameContext.h"
 #include "GameContext/Logger/ImGuiLogger.h"
 #include "GameContext/Logger/NetworkLogger.h"
-#include "GameContext/Logger/ConsoleLogger.h"
 #include "SceneGraph/SceneManager.h"
 #include "Scenes/MainMenuScene.h"
 #include <ServiceLocator/ServiceLocator.h>
 #include <Interfaces/IPhysics.h>
-
-#include "Leap/LeapClient.h"
-#include "Leap/LeapPacket.h"
-#include "Debug.h"
-#include <sstream>
 
 #if _DEBUG
 #include <vld.h>
@@ -22,13 +16,12 @@
 int main()
 {
 	leap::GameContext::GetInstance().AddLogger<leap::NetworkLogger>();
-	leap::GameContext::GetInstance().AddLogger<leap::ConsoleLogger>();
+	leap::GameContext::GetInstance().AddLogger<leap::ImGuiLogger>();
 
 	leap::LeapEngine engine{ 1280, 720, "Leap game engine" };
 
 	auto afterInitializing = []()
 		{
-			leap::Debug::LogWarning("Warning message");
 			leap::ServiceLocator::GetPhysics().SetEnabledDebugDrawing(true);
 			leap::SceneManager::GetInstance().AddScene("Test scene", unag::MainMenuScene::Load);
 			//leap::GameContext::GetInstance().GetWindow()->SetIcon("Data/Example.png");
