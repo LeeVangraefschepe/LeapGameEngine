faa018b167a5e3d0bb90601aaf2aef1214595fdc
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2024-01-25 17:09:40 +0100

    Added Particle Rendering
    Added Billboard shader
    Added ParticleRenderer component
    Added ParticleTest scene to test particle systems

---
+++ Graphics/Shaders/Billboard.cpp
@@ -0,0 +1,41 @@
+#include "Billboard.h"
+
+#include "../Shader.h"
+
+std::unique_ptr<leap::graphics::Shader, leap::graphics::ShaderDelete> leap::graphics::shaders::Billboard::GetShader()
+{
+	auto pShader{ std::unique_ptr<Shader, ShaderDelete>{ new Shader{}, ShaderDelete{} } };
+	pShader->directXVertexData = []()
+		{
+			// Create vertex layout
+			std::vector<D3D11_INPUT_ELEMENT_DESC> vertexDesc;
+
+			static constexpr unsigned int numElements{ 4 };
+			vertexDesc.resize(numElements);
+
+			vertexDesc[0].SemanticName = "POSITION";
+			vertexDesc[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+			vertexDesc[0].AlignedByteOffset = 0;
+			vertexDesc[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+
+			vertexDesc[1].SemanticName = "COLOR";
+			vertexDesc[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+			vertexDesc[1].AlignedByteOffset = 12;
+			vertexDesc[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+
+			vertexDesc[2].SemanticName = "SIZE";
+			vertexDesc[2].Format = DXGI_FORMAT_R32_FLOAT;
+			vertexDesc[2].AlignedByteOffset = 28;
+			vertexDesc[2].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+
+			vertexDesc[3].SemanticName = "ROTATION";
+			vertexDesc[3].Format = DXGI_FORMAT_R32_FLOAT;
+			vertexDesc[3].AlignedByteOffset = 32;
+			vertexDesc[3].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+
+			return vertexDesc;
+		};
+	pShader->directXDataPath = "Data/Engine/Shaders/Billboard.fx";
+
+	return std::move(pShader);
+}
